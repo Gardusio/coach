9408fb65f8f296b389130bc03e4508e68e73328f
Gardusio <fabiobarbieri.dev@gmail.com>
2024-11-25 08:28:34 +0100

    adds wearable data poc

--- src/CHA/tasks/coach/get_wearable_data.py
+++ src/CHA/tasks/coach/get_wearable_data.py
@@ -1,16 +1,22 @@
 import numpy as np
+import pandas as pd
+from typing import List, Any
 from CHA.tasks import BaseTask
 
 
 class QueryWearablesDataTask(BaseTask):
+    name: str = "get_wearable"
+    chat_name: str = "QueryWearablesDataTask"
+    description: str = "Retrieves a csv with user wearable data"
+    output_type: bool = False
+    return_direct: bool = True
+    dependencies: List[Any] = []
+    inputs: List[str] = []
+    output: List[str] = ["A np array containing user wearable data"]
+
     def _execute(self, inputs) -> str:
-        print("#" * 80)
-        print("USER WEARABLES INPUTS")
-        print(inputs)
-        print("#" * 80)
         return self.get_wearables_data()
 
-    @staticmethod
     def get_wearables_data(self):
         # 621e2fce67b776a240279baa (cleaned): 59 days
         # Array of {col_name: row_val}
